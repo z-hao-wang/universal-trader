fdc7fa0a8674d611d17bcac5431813621d6f6f6f
wanghao <whateverhow@gmail.com>
2020-06-18 15:33:39 -0700

    [master] 	modified:   ops/buildDocker.sh

--- ops/buildDocker.sh
+++ ops/buildDocker.sh
@@ -1,6 +1,7 @@
 if [ ! "$(docker ps -a | grep keyserver)" ]; then
   docker rm -f keyserver || true
-  docker run -d --restart unless-stopped -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --name=keyserver \
-  mdsol/docker-ssh-exec -server || true
+  # if you have rsa passphrase, use this line instead
+  # docker run -d --restart unless-stopped -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --name=keyserver mdsol/docker-ssh-exec -pwd [RSA_PASSPHRASE] -server || true
+  docker run -d --restart unless-stopped -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --name=keyserver mdsol/docker-ssh-exec -server || true
 fi
 docker build -t cpptrader:master .
